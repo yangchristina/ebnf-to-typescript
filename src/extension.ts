4e88438c5de01345ab7d1b57d465fbe21d7c58b4
yangchristina <chryang@students.cs.ubc.ca>
2023-03-13 11:54:02 -0700

    add validationMap

--- src/extension.ts
+++ src/extension.ts
@@ -1,40 +1,57 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
 // '{' BODY ', ' OPTIONS ', ' TRANSFORMATIONS '}'
 function checkKeyValPair(obj: unknown, isValidKey: (x: string) => boolean, isValidValue: (x: unknown) => boolean) {
-	if (!obj || typeof obj !== 'object' || Object.keys(obj).length !== 1) { return false; }
+	if (!obj || typeof obj !== 'object' || Object.keys(obj).length !== 1) {
+		return false;
+	}
 	const [key, val] = Object.entries(obj)[0];
 	return isValidKey(key) && isValidValue(val);
 }
-
-
 function isObject(x: string) {
 	return x.startsWith("'{") && x.endsWith("}'");
 }
-
+function createTargets(body: string) {
+	let str = body
+		.replace(/'/g, '')
+		.replace(/"/g, '')
+		.trim();
+	const start = str.indexOf('{');
+	const end = str.indexOf('}');
+	str = str.substring(start + 1, end);
+	const valMaps: string[] = [];
+	const targets = str
+		.split(',')
+		.map(x => x.trim())
+		.map(x => {
+			if (!x.includes(':')) {
+				valMaps.push(`${x}: is${x}`);
+				return x;
+			}
+			//  COLUMNS ,  SORT
+			// find key, find value, return '{key: value}'
+			// applykey ': {' APPLYTOKEN ':' KEY '}
+			return '{key: value}';
+		});
+	return [targets, valMaps];
+}
 function handleObject(title: string, body: string) {
-	const targets = body.substring(3, body.length - 3).split("', '").map(x => x.trim()).map(x => {
-		if (!x.includes(":")) { return x; }
-		// find key, find value, return '{key: value}'
-		// applykey ': {' APPLYTOKEN ':' KEY '}
-		return '{key: value}';
-	});
-
+	const [targets, validationMapEntries] = createTargets(body);
 	// const key = body.split(' ')[0].replace(":{", '');
 	const varName = title.toLowerCase();
 	const fn = `function is${title}(${varName}: unknown): ${varName} is ${title} {
-	const targets = [${targets.join(", ")}]
+	const targets = [${targets.map(x => `'${x}'`).join(', ')}]
 	return checkObject(${varName}, targets);
 }`;
 	// '{' BODY ', ' OPTIONS '}'
-	return [`type ${title} = ${targets.join(" & ")}`, fn];
+	return [`type ${title} = ${targets.join(' & ')}`, fn, validationMapEntries];
 }
-
 function isRegex(str: string) {
 	try {
-		if (!str.startsWith('[')) { return false; }
+		if (!str.startsWith('[')) {
+			return false;
+		}
 		const regex = new RegExp(str);
 		regex.test('test');
 		return true;
@@ -42,55 +59,48 @@ function isRegex(str: string) {
 		return false;
 	}
 }
-
 function isDefinedKeyObject(body: string) {
 	return body.endsWith("'}'") && body.includes(":{'") && !body.includes("':{'") && body.startsWith("'");
 }
-
 function handleDefinedKeyObject(title: string, body: string) {
-	const key = body.split(':')[0].substring(1).trim();
+	const key = body
+		.split(':')[0]
+		.substring(1)
+		.trim();
+	const [targets, validationMapEntries] = createTargets(body);
 	// const key = body.split(' ')[0].replace(":{", '');
 	const varName = title.toLowerCase();
-	const start = body.indexOf("{'");
-	const end = body.indexOf("'}'");
-	const types = body.substring(start + 2, end).split("', '").map(x => x.trim());
-
-	return `interface ${title} {
-	${key}: ${types.join(' & ')};
-}
-
-function is${title}(${varName}: unknown): ${varName} is ${title} {
+	return [
+		`interface ${title} {
+	${key}: ${targets.join(' & ')};
+}`,
+		`function is${title}(${varName}: unknown): ${varName} is ${title} {
+	const targets = [${targets.map(x => `'${x}'`).join(', ')}];
 	return checkKeyValPair(${varName}, (key) => key === '${key}', (v) => checkObject(${varName}, targets))
-	if (!${varName} || typeof ${varName} !== 'object') return false;
-	const entries = Object.entries(${varName});
-	if (entries.length !== 1 || entries[0][0] !== '${key}' || !entries[0][1] || typeof entries[0][1] !== 'object') return false;
-	const values = Object.entries(entries[0][1]);
-	return ${types.map(t => `is${t}({${t}: entries[0][1].${t}})`)};
+}`,
+		validationMapEntries
+	];
 }
-`;
-}
-
 // '{' FILTER '}' | '{' FILTER '}, ' FILTER_LIST
 // function isArray(body: string) {
-// 	const 
-// 	return 
+// 	const
+// 	return
 // }
-
 function isString(str: string) {
 	const s = str.trim();
-	return ((s.startsWith("'") && s.endsWith("'") && !s.substring(1, s.length - 1).includes("'"))
-		|| (s.startsWith('"') && s.endsWith('"')) && !s.substring(1, s.length - 1).includes('"'));
+	return (
+		(s.startsWith("'") && s.endsWith("'") && !s.substring(1, s.length - 1).includes("'")) ||
+		(s.startsWith('"') && s.endsWith('"') && !s.substring(1, s.length - 1).includes('"'))
+	);
 }
-
 function handleStrings(title: string, body: string, functions: string[]) {
-	const parts = body.split("|").map(x => x.trim());
+	const parts = body.split('|').map(x => x.trim());
 	const varName = title.toLowerCase();
 	functions.push(`function is${title}(${varName}: unknown): ${varName} is ${title} {
 	return ${parts.map(x => varName + ' === ' + x).join('\n\t|| ')}
 }`);
 	return `type ${title} = ${body}`;
 }
-
 function handleRegex(title: string, body: string) {
 	const varName = title.toLowerCase();
 	return `type ${title} = string;
@@ -100,32 +110,27 @@ function is${title}(${varName}: unknown): ${varName} is ${title} {
 }
 `;
 }
-
-
 // function isObject(x: string) {
 // 	const split = x.split(' ');
 // 	return isString(split[0]) && isString(split[split.length-1]) && (x.match(/:\{'/g) || []).length === 1 && (x.match(/'\}'/g) || []).length === 1;
 // }
 // function handleObject(title: string, body: string) {
-// 	const key = 
+// 	const key =
 // 	const split = body.split(' ').map(x=>{
 // 		if (isString(x)) {
 // 			return x.substring(0, x.length-1);
 // 		}
 // 	})
 // 	return `interface ${title} {
-// 	WHERE: 
+// 	WHERE:
 // }`
 // }
-
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "ebnf-to-typescript" is now active!');
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
@@ -137,21 +142,17 @@ export function activate(context: vscode.ExtensionContext) {
 	// 	// Display a message box to the user
 	// 	vscode.window.showInformationMessage('Hello World from EBNF-To-Typescript!');
 	// });
-
 	// context.subscriptions.push(disposable);
-
-	const disposable = vscode.commands.registerCommand('extension.convertEBNFToTypescript', function () {
+	const disposable = vscode.commands.registerCommand('extension.convertEBNFToTypescript', function() {
 		// Get the active text editor
 		const editor = vscode.window.activeTextEditor;
-
 		if (editor) {
 			const document = editor.document;
 			const selection = editor.selection;
-
 			// Get the word within the selection
 			const text = document.getText(selection);
 			const lines = text.split('\n');
-			const validationMap = {};
+			let validationMapEntries: string[] = [];
 			const res = Array.from(Array(lines.length));
 			const functions: string[] = [
 				`function checkKeyValPair(obj: unknown, isValidKey: (x: string) => boolean, isValidValue: (x: unknown) => boolean) {
@@ -179,51 +180,53 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	}
 	return entries.length === 0;
-}`,
-
-
+}`
 			];
 			// const lines = text.split('\n');
 			for (let i = 0; i < lines.length; i++) {
 				if (!lines[i].includes('::=')) {
 					continue;
 				}
-				let end = lines[i].indexOf("//");
-				if (end < 0) { end = lines[i].length; }
+				let end = lines[i].indexOf('//');
+				if (end < 0) {
+					end = lines[i].length;
+				}
 				const line = lines[i].substring(0, end);
-				const [title, body] = line.split("::=").map(x => x.trim());
-				const parts = body.split("|").map(x => x.trim());
+				const [title, body] = line.split('::=').map(x => x.trim());
+				const parts = body.split('|').map(x => x.trim());
 				if (parts.every(isString)) {
 					res[i] = handleStrings(title, body, functions);
-				}
-				else if (isRegex(body)) {
+				} else if (isRegex(body)) {
 					res[i] = handleRegex(title, body);
-				}
-				else if (parts.every(isString)) {
+				} else if (parts.every(isString)) {
 					const str1 = body.replace(/'\{'/g, '');
 					const str2 = str1.replace(/'\}'/g, '');
 					res[i] = `type ${title} = ${str2}`;
-				}
-				else if (isObject(body)) {
-					handleObject(title, body);
-				}
-				else if (isDefinedKeyObject(body)) {
-					res[i] = handleDefinedKeyObject(title, body);
-				}
-				else {
+				} else if (isObject(body)) {
+					const [r, fn, ve] = handleObject(title, body);
+					res[i] = r;
+					// @ts-expect-error
+					functions.push(fn);
+					validationMapEntries = [...validationMapEntries, ...ve];
+				} else if (isDefinedKeyObject(body)) {
+					// res[i] = handleDefinedKeyObject(title, body);
+					const [r, fn, ve] = handleDefinedKeyObject(title, body);
+					res[i] = r;
+					// @ts-expect-error
+					functions.push(fn);
+					validationMapEntries = [...validationMapEntries, ...ve];
+				} else {
 					res[i] = `type ${title} = {\n\ttodo\n}`;
-				};
+				}
 			}
-			const result = res.join('\n') + '\n\n' + functions.join('\n');
+			const result = `const validationMap = {${[...new Set(validationMapEntries)].join(',\n\t')}}` + '\n\n' + res.join('\n') + '\n\n' + functions.join('\n');
 			// vscode.window.showInformationMessage(result);
 			editor.edit(editBuilder => {
 				editBuilder.replace(selection, result);
 			});
 		}
 	});
-
 	context.subscriptions.push(disposable);
 }
-
 // This method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
